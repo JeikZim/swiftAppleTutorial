693f35db3e9ab2847e62038d147cdb3dc5e4ce29
JeikZim <bagautdinovwruslan@gmail.com>
2023-06-21 13:29:17 +0300

    Initial Commit

---
+++ swiftUI1Lesson/swiftUI1LessonApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUI1LessonApp.swift
+//  swiftUI1Lesson
+//
+//  Created by Руслан on 21.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct swiftUI1LessonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
